d68df6ebf8e1ba41a0bb9761bd9542122ef58f12
Markus Svensson <markus.svensson@me.com>
2014-03-03 21:42:21 +0100

    Added QJson and libmygpo-qt sources to the project
    Will investigate dynamically linked libs in the future as Jolla Harbour
    permits - build statically from source for now

--- harbour-sailpodder/harbour-sailpodder.pro
+++ harbour-sailpodder/harbour-sailpodder.pro
@@ -10,7 +10,34 @@ TARGET = harbour-sailpodder
 
 CONFIG += sailfishapp
 
-SOURCES += src/harbour-sailpodder.cpp
+SOURCES += src/harbour-sailpodder.cpp \
+    src/qjson/serializerrunnable.cpp \
+    src/qjson/serializer.cpp \
+    src/qjson/qobjecthelper.cpp \
+    src/qjson/parserrunnable.cpp \
+    src/qjson/parser.cpp \
+    src/qjson/json_scanner.cpp \
+    src/qjson/json_scanner.cc \
+    src/qjson/json_parser.cc \
+    src/libmygpo-qt/UrlBuilder.cpp \
+    src/libmygpo-qt/TagList.cpp \
+    src/libmygpo-qt/Tag.cpp \
+    src/libmygpo-qt/Settings.cpp \
+    src/libmygpo-qt/RequestHandler.cpp \
+    src/libmygpo-qt/PodcastList.cpp \
+    src/libmygpo-qt/Podcast.cpp \
+    src/libmygpo-qt/JsonCreator.cpp \
+    src/libmygpo-qt/EpisodeList.cpp \
+    src/libmygpo-qt/EpisodeActionList.cpp \
+    src/libmygpo-qt/EpisodeAction.cpp \
+    src/libmygpo-qt/Episode.cpp \
+    src/libmygpo-qt/DeviceUpdates.cpp \
+    src/libmygpo-qt/DeviceSyncResult.cpp \
+    src/libmygpo-qt/DeviceList.cpp \
+    src/libmygpo-qt/Device.cpp \
+    src/libmygpo-qt/Config.cpp \
+    src/libmygpo-qt/ApiRequest.cpp \
+    src/libmygpo-qt/AddRemoveResult.cpp
 
 OTHER_FILES += qml/harbour-sailpodder.qml \
     qml/cover/CoverPage.qml \
@@ -18,5 +45,61 @@ OTHER_FILES += qml/harbour-sailpodder.qml \
     qml/pages/SecondPage.qml \
     rpm/harbour-sailpodder.spec \
     rpm/harbour-sailpodder.yaml \
-    harbour-sailpodder.desktop
+    harbour-sailpodder.desktop \
+    src/qjson/json_scanner.yy \
+    src/qjson/json_parser.yy \
+    src/qjson/CMakeLists.txt \
+    src/libmygpo-qt/CMakeLists.txt
 
+HEADERS += \
+    src/qjson/FlexLexer.h \
+    src/qjson/stack.hh \
+    src/qjson/serializerrunnable.h \
+    src/qjson/serializer.h \
+    src/qjson/qobjecthelper.h \
+    src/qjson/qjson_export.h \
+    src/qjson/qjson_debug.h \
+    src/qjson/position.hh \
+    src/qjson/parserrunnable.h \
+    src/qjson/parser.h \
+    src/qjson/parser_p.h \
+    src/qjson/location.hh \
+    src/qjson/json_scanner.h \
+    src/qjson/json_parser.hh \
+    src/libmygpo-qt/UrlBuilder.h \
+    src/libmygpo-qt/TagList.h \
+    src/libmygpo-qt/TagList_p.h \
+    src/libmygpo-qt/Tag.h \
+    src/libmygpo-qt/Tag_p.h \
+    src/libmygpo-qt/Settings.h \
+    src/libmygpo-qt/Settings_p.h \
+    src/libmygpo-qt/RequestHandler.h \
+    src/libmygpo-qt/PodcastList.h \
+    src/libmygpo-qt/PodcastList_p.h \
+    src/libmygpo-qt/Podcast.h \
+    src/libmygpo-qt/Podcast_p.h \
+    src/libmygpo-qt/mygpo_export.h \
+    src/libmygpo-qt/JsonCreator.h \
+    src/libmygpo-qt/EpisodeList.h \
+    src/libmygpo-qt/EpisodeList_p.h \
+    src/libmygpo-qt/EpisodeActionList.h \
+    src/libmygpo-qt/EpisodeActionList_p.h \
+    src/libmygpo-qt/EpisodeAction.h \
+    src/libmygpo-qt/EpisodeAction_p.h \
+    src/libmygpo-qt/Episode.h \
+    src/libmygpo-qt/Episode_p.h \
+    src/libmygpo-qt/DeviceUpdates.h \
+    src/libmygpo-qt/DeviceUpdates_p.h \
+    src/libmygpo-qt/DeviceSyncResult.h \
+    src/libmygpo-qt/DeviceSyncResult_p.h \
+    src/libmygpo-qt/DeviceList.h \
+    src/libmygpo-qt/DeviceList_p.h \
+    src/libmygpo-qt/Device.h \
+    src/libmygpo-qt/Device_p.h \
+    src/libmygpo-qt/Config.h \
+    src/libmygpo-qt/Config_p.h \
+    src/libmygpo-qt/ApiRequest.h \
+    src/libmygpo-qt/ApiRequest_p.h \
+    src/libmygpo-qt/AddRemoveResult.h \
+    src/libmygpo-qt/AddRemoveResult_p.h \
+    src/libmygpo-qt/Version.h
---
+++ harbour-sailpodder/src/libmygpo-qt/JsonCreator.cpp
@@ -0,0 +1,214 @@
+/***************************************************************************
+* This file is part of libmygpo-qt                                         *
+* Copyright (c) 2010 - 2013 Stefan Derkits <stefan@derkits.at>             *
+* Copyright (c) 2010 - 2011 Christian Wagner <christian.wagner86@gmx.at>   *
+* Copyright (c) 2010 - 2011 Felix Winter <ixos01@gmail.com>                *
+*                                                                          *
+* This library is free software; you can redistribute it and/or            *
+* modify it under the terms of the GNU Lesser General Public               *
+* License as published by the Free Software Foundation; either             *
+* version 2.1 of the License, or (at your option) any later version.       *
+*                                                                          *
+* This library is distributed in the hope that it will be useful,          *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU        *
+* Lesser General Public License for more details.                          *
+*                                                                          *
+* You should have received a copy of the GNU Lesser General Public         *
+* License along with this library; if not, write to the Free Software      *
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 *
+* USA                                                                      *
+***************************************************************************/
+
+#include <QVariant>
+#include <QList>
+#include <QUrl>
+#include <QString>
+#include <QDateTime>
+
+#include "../qjson/serializer.h"
+
+#include "JsonCreator.h"
+
+using namespace mygpo;
+
+static qulonglong c_maxlonglong = (2^64)-1;
+
+QByteArray JsonCreator::addRemoveSubsToJSON( const QList< QUrl >& add, const QList< QUrl >& remove )
+{
+    QJson::Serializer serializer;
+    QVariantMap jsonData;
+    QVariant addVar( urlListToQVariantList( add ) );
+    QVariant removeVar( urlListToQVariantList( remove ) );
+    jsonData.insert( QString( QLatin1String( "add" ) ), addVar );
+    jsonData.insert( QString( QLatin1String( "remove" ) ), removeVar );
+    QByteArray jsonByteArray = serializer.serialize( QVariant( jsonData ) );
+    return jsonByteArray;
+}
+
+QByteArray JsonCreator::saveSettingsToJSON( const QMap< QString, QVariant >& set, const QList< QString >& remove )
+{
+    QJson::Serializer serializer;
+    QVariantMap jsonData;
+    //QVariant setVar(stringMapToQVariantMap(set));
+    QVariant removeVar( stringListToQVariantList( remove ) );
+    jsonData.insert( QString( QLatin1String( "set" ) ), set );
+    jsonData.insert( QString( QLatin1String( "remove" ) ), removeVar );
+    QByteArray jsonByteArray = serializer.serialize( QVariant( jsonData ) );
+    return jsonByteArray;
+}
+
+QByteArray JsonCreator::episodeActionListToJSON( const QList<EpisodeActionPtr>& episodeActions )
+{
+    QJson::Serializer serializer;
+    QVariantList jsonData;
+
+    foreach( const EpisodeActionPtr episodeAction, episodeActions )
+    {
+        jsonData.append( episodeActionToQVariantMap( episodeAction ) );
+    }
+
+    QByteArray jsonByteArray = serializer.serialize( QVariant( jsonData ) );
+    return jsonByteArray;
+}
+
+QByteArray JsonCreator::renameDeviceStringToJSON( const QString& caption, const QString& type )
+{
+    QJson::Serializer serializer;
+    QVariantMap jsonData;
+    QVariant captionVar( caption );
+    QVariant typeVar( type );
+    jsonData.insert( QString( QLatin1String( "caption" ) ), captionVar );
+    jsonData.insert( QString( QLatin1String( "type" ) ), typeVar );
+    QByteArray jsonByteArray = serializer.serialize( QVariant( jsonData ) );
+    return jsonByteArray;
+
+}
+
+QByteArray JsonCreator::deviceSynchronizationListsToJSON(const QList< QStringList >& synchronize, const QList< QString >& stopSynchronize)
+{
+    QString syncVar;
+    if (synchronize.size() != 0)
+    {
+        syncVar.append(QLatin1String("["));
+        foreach( const QStringList& syncEntry, synchronize )
+        {
+            QString syncEntryStr;
+            if (syncEntry.size() != 0) {
+                syncEntryStr.append(QLatin1String("["));
+                foreach( const QString& str, syncEntry) {
+                    syncEntryStr.append(QString(QLatin1String("\"") + str + QLatin1String("\",")));
+                }
+                syncEntryStr.replace(syncEntryStr.size()-1,1,QLatin1String("]"));
+            }
+            else {
+                syncEntryStr.append(QLatin1String("[ ]"));
+            }
+            syncVar.append(syncEntryStr);
+            syncVar.append(QLatin1String(","));
+        }
+        syncVar.replace(syncVar.size()-1,1,QLatin1String("]"));
+    }
+    else
+    {
+        syncVar.append(QLatin1String("[ ]"));
+    }
+    QString stopVar;
+    if (stopSynchronize.size() != 0)
+    {
+        stopVar.append(QLatin1String("["));
+        foreach( const QString& str, stopSynchronize) {
+            stopVar.append(QString(QLatin1String("\"") + str + QLatin1String("\",")));
+        }
+        stopVar.replace(stopVar.size()-1,1,QLatin1String("]"));
+    }
+    else
+    {
+        stopVar.append(QLatin1String("[ ]"));
+    }
+    QString jsonStr(QLatin1String("{\"synchronize\" : "));
+    jsonStr.append(syncVar);
+    jsonStr.append(QLatin1String(" ,\"stop-synchronize\" : "));
+    jsonStr.append(stopVar);
+    jsonStr.append(QLatin1String(" }\n"));
+    return jsonStr.toLocal8Bit();
+}
+
+QVariantList JsonCreator::urlListToQVariantList( const QList< QUrl >& urls )
+{
+    QVariantList list;
+    foreach( const QUrl & url, urls )
+    {
+        QVariant var( url.toString() );
+        if( !list.contains( var ) )
+            list.append( var );
+    }
+    return list;
+}
+
+QVariantList JsonCreator::stringListToQVariantList( const QList< QString >& strings )
+{
+    QVariantList list;
+    foreach( const QString & str, strings )
+    {
+        QVariant var( str );
+        list.append( var );
+    }
+    return list;
+}
+
+QVariantMap mygpo::JsonCreator::stringMapToQVariantMap( const QMap< QString, QString >& stringmap )
+{
+    QVariantMap map;
+    foreach( const QString & str, stringmap.keys() )
+    {
+        map.insert( str, QVariant( stringmap.value( str ) ) );
+    }
+    return map;
+}
+
+QVariantMap JsonCreator::episodeActionToQVariantMap( const EpisodeActionPtr episodeAction )
+{
+    QVariantMap map;
+    map.insert( QLatin1String( "podcast" ), episodeAction->podcastUrl() );
+    map.insert( QLatin1String( "episode" ), episodeAction->episodeUrl() );
+    if( episodeAction->deviceName().compare( QLatin1String( "" ) ) != 0 )
+        map.insert( QLatin1String( "device" ), episodeAction->deviceName() );
+
+    EpisodeAction::ActionType actionType = episodeAction->action();
+    if( actionType == EpisodeAction::New )
+        map.insert( QLatin1String( "action" ), QLatin1String( "new" ) );
+    else if( actionType == EpisodeAction::Delete )
+        map.insert( QLatin1String( "action" ), QLatin1String( "delete" ) );
+    else if( actionType == EpisodeAction::Play )
+        map.insert( QLatin1String( "action" ), QLatin1String( "play" ) );
+    else if( actionType == EpisodeAction::Download )
+        map.insert( QLatin1String( "action" ), QLatin1String( "download" ) );
+    else if( actionType == EpisodeAction::Flattr )
+        map.insert( QLatin1String( "action" ), QLatin1String( "flattr" ) );
+
+    if( episodeAction->timestamp() != 0 ) {
+#if QT_VERSION >= 0x040700
+        QDateTime dateTime = QDateTime::fromMSecsSinceEpoch(episodeAction->timestamp() );
+#else
+        QDateTime dateTime = QDateTime::fromTime_t(episodeAction->timestamp() / 1000 );
+        QTime time = dateTime.time();
+        time.addMSecs(episodeAction->timestamp() % 1000 );
+        dateTime.setTime(time);
+#endif
+        map.insert( QLatin1String( "timestamp" ), dateTime.toString(Qt::ISODate) );
+    }
+    if( actionType == EpisodeAction::Play )
+    {
+        if ( episodeAction->position() != c_maxlonglong )
+        {
+            map.insert( QLatin1String( "position" ), episodeAction->position() );
+            if ( episodeAction->started() != c_maxlonglong && episodeAction->total() != c_maxlonglong )
+            {
+                map.insert( QLatin1String( "started" ), episodeAction->started() );
+                map.insert( QLatin1String( "total" ), episodeAction->total() );
+            }
+        }
+    }
+    return map;
+}
